a43b2ce106d9712634b3852304829c9698ab2c24
arielbk <buchwald.kearney@gmail.com>
2023-10-01 15:56:27 +0200

    feat: add ocean landing vid

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,35 @@
+import { auth } from '@clerk/nextjs';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  let href = userId ? '/journal' : '/new-user';
+
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-black text-white">
-      <div className="w-full max-w-[600px]">
-        <h1 className="mb-2 text-6xl">Ocean</h1>
-        <p className="mb-12 text-2xl text-white/60">
-          Track your mood automatically over time automatically with the power
-          of AI
-        </p>
-        <div>
-          <Link
-            className="rounded-lg bg-blue-500 px-4 py-2 text-xl transition-colors hover:bg-blue-400"
-            href="/journal"
-          >
-            Get started
-          </Link>
-        </div>
-      </div>
+    <div className="relative flex h-screen w-screen items-center justify-center overflow-hidden bg-black text-white">
+      <video
+        src="https://www.shutterstock.com/shutterstock/videos/1092713723/preview/stock-footage-amazing-top-down-aerial-view-of-the-powerful-deep-blue-ocean-waves-during-the-monsoon-season.webm"
+        className="absolute z-0 min-h-full w-auto min-w-full max-w-none"
+        autoPlay
+        muted
+        loop
+      />
+      <div className="absolute z-0 min-h-full w-auto min-w-full max-w-none bg-black/90" />
+      <main className="z-10 w-full max-w-[600px]">
+        <h1 className="hover:drop-shadow-glow mb-4 text-7xl font-light">
+          Ocean
+        </h1>
+        <h2 className="mb-16 text-2xl font-light text-white/60">
+          Discover patterns, gain insights, and build a happier, more balanced
+          life
+        </h2>
+        <Link
+          className="hover:drop-shadow-glow rounded-lg bg-blue-500  px-5 py-3 text-xl font-light transition-all hover:bg-blue-400"
+          href={href}
+        >
+          Get started
+        </Link>
+      </main>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      dropShadow: {
+        glow: [
+          '0 0px 40px rgba(255,255, 255, 0.225)',
+          '0 0px 85px rgba(255, 255,255, 0.2)',
+        ],
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
